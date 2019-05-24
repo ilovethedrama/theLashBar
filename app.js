bc626f130f483b245746b505f8e754cd06a5996c
Status100 <bluemagicuk@yahoo.co.uk>
2019-05-24 10:41:20 +0100

    refactoring app - add app.js config file to set up server,  initial home and appointment routes

---
+++ app.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var bodyparser = require('body-parser');
+
+var app = express();
+var port = process.env.port || 3000;
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + '/styles/'));
+app.use(bodyparser.urlencoded({extended: true}));
+//this is a way of using, requiring and running at the same time. 
+//It needs 3 options, the secret can be literally anything, it is used to code and decode web sessions
+app.use(require('express-session')({
+    secret: 'Mama Mia Thats a Spicy Meatball!',
+    resave: false,
+    saveUninitialized: false
+}));
+
+
+app.get('/', function(req, res){
+    res.render('main');
+});
+
+app.get('/appointment', function(req, res){
+    res.render('appointment');
+});
+
+app.listen(port, function(){
+    console.log('You rockin now on port ' + port);
+});
+
+var http = require('http');
+var servz = http.createServer(function(req, res){
+    res.writeHead(200, {'Content-Type':'text/plain'});
+    res.end('Sup bitches?');
+})
+
+servz.listen(3001, '127.0.0.1');
