914da3e865b42d998c423526ee36d7b757787c59
Status100 <bluemagicuk@yahoo.co.uk>
2019-05-28 02:58:45 +0100

    update config and basic routes for app

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-var express = require('express');
-var bodyparser = require('body-parser');
-
-var app = express();
-var port = process.env.port || 3000;
+const port = process.env.port || 3000,
+    bodyparser = require('body-parser'),
+    mongoose = require('mongoose'),
+    express = require('express'),
+    app = express();
 
 app.set('view engine', 'ejs');
 
-// app.use(express.static(__dirname + '/styles/'));
 app.use(bodyparser.urlencoded({
     extended: true
 }));
@@ -18,20 +17,34 @@ app.use(require('express-session')({
     saveUninitialized: false
 }));
 
-console.log(__dirname + '/Media/');
-console.log(__dirname + '/styles/');
-
 app.use(express.static(__dirname + '/styles/'));
 app.use(express.static(__dirname + '/Media/'));
 app.use(express.static(__dirname + '/JS/'));
 
-app.get('/', function (req, res) {
-    res.render('main');
+mongoose.connect('mongodb://localhost/lashbar', {
+    useNewUrlParser: true
+});
+const mongCon = mongoose.connection;
+mongCon.on('connected', function () {
+    console.log('beep boop \ndatabase mainframe syncroniiiiiiized');
 });
 
-app.get('/appointment', function (req, res) {
-    res.render('appointment');
+mongCon.on('disconnected', function () {
+    console.log('you\'re off the database now');
 });
+
+// mongCon.on('error', function () {
+//     console.log(error, 'ohhh shit we got incoming, \n there\'s an error!!');
+// });
+
+
+app.get("/", function(req, res) {
+    res.render("main");
+  });
+  
+  app.get("/appointment", function(req, res) {
+    res.render("appointment");
+  });
 
 app.listen(port, function () {
     console.log('You rockin now on port ' + port);
@@ -44,5 +57,47 @@ var servz = http.createServer(function (req, res) {
     });
     res.end('Sup bitches?');
 });
+
+
+var customerSchema = new mongoose.Schema({
+    name: {
+        firstName: String,
+        middleName: String,
+        lastName: String
+    },
+    mobile: String,
+    email: String,
+    visit: {
+        treatment: String,
+        allergies: String,
+        time: Number,
+        date: Date,
+        howManyTimes: Number
+    }
+});
+
+var Customer = mongoose.model('Customer', customerSchema);
+// this makes a model based on the customer schema with methods on it so that you can do Customer.find etc
+
+app.post('/appointment', function (req, res) {
+    console.log('data was sent i think!');
+    res.redirect('/');
+});
+
+// Customer.create({
+//     name: {
+//         firstName: 'Marcia',
+//         lastName: 'Henriques',
+//     },
+//     mobile: '07947733333',
+//     email: 'cheerios@cheeryGood.com'
+// }, function (err, personFile) {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log(personFile);
+//         console.log('newly created file for a customer');
+//     }
+// });
 
 servz.listen(3001, '127.0.0.1');
