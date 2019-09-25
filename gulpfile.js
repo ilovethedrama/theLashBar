5811f903c70de1f86f233a2bc6af9cc56a02e040
Status100 <bluemagicuk@yahoo.co.uk>
2019-09-25 21:15:52 +0100

    add a test function and a sass to css function to gulp file to automate

--- gulpfile.js
+++ gulpfile.js
@@ -1,63 +1,56 @@
-const {
-  watch,
-  series,
-  parallel,
-  src,
-  dest,
-  gulp
-} = require('gulp');
+"use strict";
 
-// var { gulp, series, dest }    = require('gulp'),
+const gulp = require("gulp"),
+    sass = require("gulp-sass"),
+    autoPrefixer = require("gulp-autoprefixer"),
+    browserSync = require("browser-sync").create(),
+    plumber = require("gulp-plumber");
 
-// var sass    = require('gulp-sass'),
-//     open    = require('gulp-open'),
-//     connect = require('gulp-connect');
+var test = async () => {
+    console.log(
+        `
+    sup 
+    sup 
+    sup
+    `
+    );
+};
+test.description = "test to make sure gulp works";
 
-// gulp.task('micCheck', function(fin) {
-//     console.log(
-//         'Yo, microphone check one two what is this,' + '\n' + 
-//         'The five foot assassin with the ruffneck business,' + '\n' +
-//         'I float like gravity, never had a cavity,' + '\n' +
-//         'Got more rhymes than the Winans got family');
-//         fin();
-// });
+var scssToCss = () => {
+    return gulp
+        .src("./styles/SCSS/lashbar.scss")
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(autoPrefixer())
+        .pipe(gulp.dest("./styles/CSS"))
+        .pipe(
+            browserSync.stream({
+                match: "**/*.css"
+            })
+        );
+};
 
-// gulp.task('sass', function() {
-//     // this searches within the styles/scss folder for any files ending scss or sass
-//     return gulp.src('styles/scss/*.+(scss|sass)')
-//     .pipe(sass())
-//     .pipe(gulp.dest('styles/css'),
-//     gulp.watch('styles/scss/*.+(scss|sass)', gulp.series('sass'))
-//     )
-// });
+scssToCss.description =
+    "changes scss to css and adds autoprefixes for browser support";
 
+var bSync = done => {
+    browserSync.init({
+        server: {
+            baseDir: "app.js"
+        },
+        // files: ["views/*.ejs", "views/partials/.*ejs", "styles/SCSS"],
+        port: 3000
+    });
+    done();
+};
 
-// Launch Chrome web browser
-// https://www.npmjs.com/package/gulp-open
+bSync.description = "allows for live browser view of file as changes are made";
 
-// function openBrowser(done) {
-//     var options = {
-//       uri: 'http://localhost:8080'
-//     };
-//     return src('./')
-//     .pipe(open(options));
-//     done();
-//   }
+var watcher = () => {
+    gulp.watch("styles/SCSS/*.scss").on("change", scssToCss);
+    gulp.watch("views/*.ejs").on("change", browserSync.reload);
+};
 
-// Gulp plugin to run a webserver (with LiveReload)
-// https://www.npmjs.com/package/gulp-connect
-
-// function server(done) {
-//   return connect.server({
-//     root: './',
-//     port: 8080,
-//     debug: true,
-//   });
-//   done();
-// }
-
-// Default Gulp command
-// exports.default = series(openBrowser, server);
-
-// the (old) way to watch a file in gulp 3.0 Version 4 is above ^^
-// gulp.watch('styles/scss/*.+(scss|sass)', ['sass']); 
+gulp.task("tester", gulp.series(test));
+gulp.task("default", gulp.parallel(bSync, watcher));
