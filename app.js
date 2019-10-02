20de585e6c9af15dda8973dc176827adc5d8c948
Status100 <bluemagicuk@yahoo.co.uk>
2019-10-02 23:28:34 +0100

    update filepath for css

--- app.js
+++ app.js
@@ -35,6 +35,7 @@ app.use(express.static(__dirname + "/styles/CSS/"));
 app.use(express.static(__dirname + "/Media/"));
 app.use(express.static(__dirname + "/JS/"));
 
+console.log(`the path is`, __dirname + "/styles/CSS/");
 
 
 const nexmo = new Nexmo({
