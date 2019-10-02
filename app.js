d45acff6709c64871d33f7ce8e37226e2ad29755
Status100 <bluemagicuk@yahoo.co.uk>
2019-10-02 19:05:28 +0100

    initialize nexmo to allow text notifications and use api key and secret as env variables

--- app.js
+++ app.js
@@ -1,107 +1,163 @@
-const port = process.env.port || 3000,
-    bodyparser = require('body-parser'),
-    mongoose = require('mongoose'),
-    express = require('express'),
-    app = express();
+const port = process.env.PORT || 3000,
+  bodyparser = require("body-parser"),
+  mongoose = require("mongoose"),
+  express = require("express"),
+  Nexmo = require('nexmo'),
+  app = express();
 
-app.set('view engine', 'ejs');
+// this tells the server to load anything thats in an env file into an environment variable
+require('dotenv').config();
 
-app.use(bodyparser.urlencoded({
+app.set("view engine", "ejs");
+// Initialize the Nexmo deets in env var
+const nexmoAPI_KEY = process.env.API_KEY;
+const nexmoAPI_SECRET = process.env.API_SECRET;
+
+
+
+app.use(
+  bodyparser.urlencoded({
     extended: true
-}));
-//this is a way of using, requiring and running at the same time. 
+  })
+);
+//this is a way of using, requiring and running at the same time.
 //It needs 3 options, the secret can be literally anything, it is used to code and decode web sessions
-app.use(require('express-session')({
-    secret: 'Mama Mia Thats a Spicy Meatball!',
+app.use(
+  require("express-session")({
+    secret: "Mama Mia Thats a Spicy Meatball!",
     resave: false,
     saveUninitialized: false
-}));
+  })
+);
 
-app.use(express.static(__dirname + '/styles/'));
-app.use(express.static(__dirname + '/Media/'));
-app.use(express.static(__dirname + '/JS/'));
+app.use(express.static(__dirname + "/styles/"));
+app.use(express.static(__dirname + "/Media/"));
+app.use(express.static(__dirname + "/JS/"));
 
-mongoose.connect('mongodb://localhost/lashbar', {
-    useNewUrlParser: true
+
+
+const nexmo = new Nexmo({
+  apiKey: nexmoAPI_KEY,
+  apiSecret: nexmoAPI_SECRET,
+}, {
+  debug: true
 });
+mongoose.connect("mongodb://localhost/lashB", {
+  useNewUrlParser: true
+});
+
 const mongCon = mongoose.connection;
-mongCon.on('connected', function () {
-    console.log('beep boop \ndatabase mainframe syncroniiiiiiized');
+
+mongCon.on("connected", function () {
+  console.log(`beep boop 
+    database mainframe syncroniiiiiiized`);
 });
 
-mongCon.on('disconnected', function () {
-    console.log('you\'re off the database now');
+mongCon.on("disconnected", function () {
+  console.log(`You're off the database now...Daniel San.`);
 });
 
-// mongCon.on('error', function () {
-//     console.log(error, 'ohhh shit we got incoming, \n there\'s an error!!');
-// });
-
+mongCon.on("error", function () {
+  console.log(`We've got company...there's an error!!!`);
+});
 
 app.get("/", function (req, res) {
-    res.render("main");
+  res.render("main");
 });
 
 app.get("/appointment", function (req, res) {
-    res.render("appointment");
+  res.render("appointment");
 });
 
 app.get("/appointmentConfirmation", function (req, res) {
-    res.render("appConfirmation");
+  res.render("appConfirmation");
 });
 
-app.listen(port, function () {
-    console.log('You rockin now on port ' + port);
+app.get("/blogHome", function (req, res) {
+  res.render("blogHome");
 });
+// var http = require('http');
+// var servz = http.createServer(function (req, res) {
+//     res.writeHead(200, {
+//         'Content-Type': 'text/plain'
+//     });
+//     res.end('Sup bitches?');
+// });
 
-var http = require('http');
-var servz = http.createServer(function (req, res) {
-    res.writeHead(200, {
-        'Content-Type': 'text/plain'
-    });
-    res.end('Sup bitches?');
-});
+// servz.listen(3001, '127.0.0.1');
+
+
+
+
+const from = 'The Lashbar';
+const to = '447525618042';
+const text = `Thanks for placing your booking, we'll be in touch to confirm shortly. Stay classy bitch -LashBar`;
+const options = {
+  "type": "unicode"
+}
 
 
 var customerSchema = new mongoose.Schema({
-    name: {
-        firstName: String,
-        middleName: String,
-        lastName: String
-    },
-    mobile: String,
-    email: String,
-    visit: {
-        treatment: String,
-        allergies: String,
-        time: Number,
-        date: Date,
-        howManyTimes: Number
-    }
+  firstName: String,
+  lastName: String,
+  mobile: String,
+  email: String,
+  treatment: String,
+  time: String,
+  date: Date
 });
 
-var Customer = mongoose.model('Customer', customerSchema);
+var Customer = mongoose.model("Customer", customerSchema, "Customer");
 // this makes a model based on the customer schema with methods on it so that you can do Customer.find etc
 
 app.post('/appointment', function (req, res) {
-    console.log('data was sent i think!');
-    res.redirect('/');
+  var firstName = req.body.firstName,
+    lastName = req.body.lastName,
+    mobile = req.body.mobile,
+    email = req.body.email,
+    treatment = req.body.treatment,
+    time = req.body.time,
+    date = req.body.date;
+  var deetz = {
+    date: date,
+    time: time,
+    treatment: treatment,
+    email: email,
+    mobile: mobile,
+    firstName: firstName,
+    lastName: lastName
+  }
+
+  Customer.collection.insertOne(deetz, function (err, info) {
+    if (err) {
+      console.log(err, `something went wrong`);
+    } else {
+      console.log(`this is the info passed through: `, info);
+      console.log('newly created file for a customer');
+    }
+  });
+  // Customer.create(deetz, function (err, info) {
+  //   if (err) {
+  //     console.log(err, `something went wrong`);
+  //   } else {
+  //     console.log(info);
+  //     console.log('newly created file for a customer');
+  //   }
+  // });
+
+  // this is the unicode equivalent of the 100 emoji
+  const hunna = 'U+1F4AF'
+  nexmo.message.sendSms(from, to, `LASHBAR NYC: Thanks ${firstName} we'll be in touch shortly to confirm your ${treatment} treatment. x`);
+
+  res.render('appConfirmation', {
+      firstName: firstName
+      /*the first 'firstName' this refers to what it'll be called in appConfirmation, 
+      and the second is what its called within the post route*/
+    }
+
+  );
 });
 
-// Customer.create({
-//     name: {
-//         firstName: 'Marcia',
-//         lastName: 'Henriques',
-//     },
-//     mobile: '07947733333',
-//     email: 'cheerios@cheeryGood.com'
-// }, function (err, personFile) {
-//     if (err) {
-//         console.log(err);
-//     } else {
-//         console.log(personFile);
-//         console.log('newly created file for a customer');
-//     }
-// });
-
-servz.listen(3001, '127.0.0.1');
+app.listen(port, function () {
+  console.log(`You rockin now on port ${port}`);
+});
