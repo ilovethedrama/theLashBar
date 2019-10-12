27dc4df651a33c85387a90f7f5fb5b2a2b8a277b
Status100 <bluemagicuk@yahoo.co.uk>
2019-10-12 13:39:10 +0100

    Fuck bro. So. Set gulp file to start server, then open browserSync and while watching JS, SCSS and EJS files to inject the changes into browser and also restart the server with nodemon and also compile SCSS to CSS. Bosh.

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,15 @@
 "use strict";
 
-const gulp = require("gulp"),
-    sass = require("gulp-sass"),
+const browserSync = require("browser-sync").create(),
     autoPrefixer = require("gulp-autoprefixer"),
-    browserSync = require("browser-sync").create(),
-    exec = require('child_process').exec,
+    uglifycss = require('gulp-uglifycss'),
     plumber = require("gulp-plumber"),
+    nodemon = require("gulp-nodemon"),
     notify = require('gulp-notify'),
-    nodemon = require("gulp-nodemon");
+    sass = require("gulp-sass"),
+    gulp = require("gulp");
 
-
-var test = async () => {
-    console.log(
-        `
-    sup 
-    sup 
-    sup
-    `
-    );
-};
-test.description = "test to make sure gulp works";
-
-var scssToCss = () => {
+const scssToCss = () => {
     return gulp
         .src("./styles/SCSS/lashbar.scss")
         .pipe(plumber())
@@ -33,48 +21,55 @@ var scssToCss = () => {
                 match: "**/*.css"
             })
         );
+
 };
-
-var closeServ = function shower(done) {
-    exec('killall mongod', function (err, stdout, stderr) {
-        console.log(stdout);
-        console.log(stderr);
-        done(err);
-    })
-}
-
-var servah = function endGame(done) {
-    exec('node app.js', function (err, stdout, stderr) {
-        console.log(stdout);
-        console.log(stderr);
-        done(err);
-    });
-    // exec('mongod', function (err, stdout, stderr) {
-    //     console.log(stdout);
-    //     console.log(stderr);
-    //     done(err);
-    // });
-}
-
 scssToCss.description =
     "changes scss to css and adds autoprefixes for browser support";
 
-var bSync = done => {
+
+
+const distill = () => {
+    return gulp.src('./styles/CSS/lashbar.css')
+        .pipe(uglifycss({
+            'uglyComments': true
+        }))
+        .pipe(gulp.dest('./producki/'))
+}
+
+let thePausinator;
+
+const holdIt = () => {
+    thePausinator = setTimeout(bSync, 3000);
+}
+
+const bSync = () => {
     browserSync.init({
-        proxy: 'localhost:3000'
+        proxy: 'localhost:3000',
+        port: 3003,
+        online: true,
+        tunnel: 'yoMamaHouse'
 
     });
-    done();
+    // done();
 };
+
+
+
+
 
 bSync.description = "allows for live browser view of file as changes are made";
 
-var watcher = () => {
+var watcher = (done) => {
     gulp.watch("styles/SCSS/*.scss").on("change", scssToCss);
     gulp.watch("views/partials/*.ejs").on("change", browserSync.reload);
+    done();
 };
+watcher.description = "watches both the ejs and scss files and triggers a browser reload or css injection";
 
-var server = function (done) {
+
+
+
+const server = function (done) {
     // configure nodemon
     nodemon({
         // the script to run the app
@@ -86,29 +81,20 @@ var server = function (done) {
     }).on('restart', () => {
         gulp.src('app.js')
             // I've added notify, which displays a message on restart. Was more for me to test so you can remove this
-            .pipe(notify('Running the start tasks and stuff'));
+            .pipe(notify('restarting the server file now'));
     });
     done();
 };
-// var server = function () {
-//     // configure nodemon
-//     nodemon({
-//         // the script to run the app
-//         script: 'app.js',
-//         // this listens to changes in any of these files/routes and restarts the application
-//         watch: ["app.js", "views/*.ejs", 'styles/SCSS/*.scss'],
-//         ext: 'js'
-//         // Below i'm using es6 arrow functions but you can remove the arrow and have it a normal .on('restart', function() { // then place your stuff in here }
-//     }).on('restart', () => {
-//         gulp.src('app.js')
-//             // I've added notify, which displays a message on restart. Was more for me to test so you can remove this
-//             .pipe(notify('Running the start tasks and stuff'));
-//     });
-// };
 
-gulp.task("tester", gulp.series(scssToCss));
-gulp.task("tester2", gulp.series(servah));
-gulp.task("tester3", gulp.series(server));
-gulp.task("tester3a", gulp.series(bSync));
-gulp.task("default", gulp.parallel(server, scssToCss, watcher));
-gulp.task("wine", gulp.series(gulp.parallel(bSync, scssToCss, watcher), server));
+
+
+
+
+// this works
+gulp.task("tester3", gulp.parallel(server, bSync));
+gulp.task('now', gulp.parallel(gulp.series(scssToCss), watcher));
+
+/*This starts the server and also browserSync and then watches for 
+changes to the scss files and ejs files and updates the browser if
+ either one changes and also injects css when scss is ListeningStateChangedEvent. SICCKKK */
+gulp.task("default", gulp.parallel(server, holdIt, gulp.parallel(gulp.series(scssToCss), watcher)));
