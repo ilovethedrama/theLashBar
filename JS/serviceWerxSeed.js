a66f970b5c716bceadb7fa049c95a0f94bb700d4
Status100 <bluemagicuk@yahoo.co.uk>
2019-10-26 15:59:47 +0100

    update path to service worker

--- JS/serviceWerxSeed.js
+++ JS/serviceWerxSeed.js
@@ -2,7 +2,7 @@
 basically checking to make sure its supported in the current browser */
 
 
-var path = '../../serviceWerx.js';
+// var path = '/serviceWerx.js';
 
 console.log(navigator);
 if (navigator.serviceWorker) {
@@ -10,10 +10,11 @@ if (navigator.serviceWorker) {
      so the then function will be resolved once the register part is sorted. The async task creates an implied/implicit 'promise', this promise, 
      or guarantee is handled by 'then' and 'catch', then to deal with a successful completion and catch to deal with the error. 
      If successful the promise takes in a registration object which i've called registeredSW */
-    navigator.serviceWorker.register(path).then((registeredSW) => {
+    navigator.serviceWorker.register('/serviceWerx.js').then((registeredSW) => {
         console.log('Yeah bro its registered now', registeredSW)
-        console.log(path);  
+        // console.log(path);  
     }).catch((err) => {
         console.log(`Nah bro, couldn\'t register it still! ${err}`);
+        // console.log(path);  
     })
 }
