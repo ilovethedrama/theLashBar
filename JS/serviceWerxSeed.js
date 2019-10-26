eda2f80c02277ee1fb870198e8dd4160c66a9dca
Status100 <bluemagicuk@yahoo.co.uk>
2019-10-26 14:50:05 +0100

    store path as variable to see where  service worker is pointing to

--- JS/serviceWerxSeed.js
+++ JS/serviceWerxSeed.js
@@ -1,15 +1,19 @@
 /*register service worker fo' PWA. Navigator = browser, 
 basically checking to make sure its supported in the current browser */
 
+
+var path = '/../serviceWerx.js';
+
 console.log(navigator);
 if (navigator.serviceWorker) {
     /* This is an asynchronous task as it takes time to complete and
      so the then function will be resolved once the register part is sorted. The async task creates an implied/implicit 'promise', this promise, 
      or guarantee is handled by 'then' and 'catch', then to deal with a successful completion and catch to deal with the error. 
      If successful the promise takes in a registration object which i've called registeredSW */
-    navigator.serviceWorker.register('/../serviceWerx.js').then((registeredSW) => {
+    navigator.serviceWorker.register(path).then((registeredSW) => {
         console.log('Yeah bro its registered now', registeredSW)
+        console.log(path);  
     }).catch((err) => {
-        console.log(`Nah bro, couldn\'t register it still! ${err}`)
+        console.log(`Nah bro, couldn\'t register it still! ${err}`);
     })
 }
