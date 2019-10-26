223345a46acb134acba0546f009285bfd444c4b3
Status100 <bluemagicuk@yahoo.co.uk>
2019-10-26 21:14:07 +0100

    set service worker file to root in path usin tilda ~ sign in the register file

--- JS/serviceWerxSeed.js
+++ JS/serviceWerxSeed.js
@@ -6,7 +6,7 @@ if (navigator.serviceWorker) {
      so the then function will be resolved once the register part is sorted. The async task creates an implied/implicit 'promise', this promise, 
      or guarantee is handled by 'then' and 'catch', then to deal with a successful completion and catch to deal with the error. 
      If successful the promise takes in a registration object which i've called registeredSW */
-    navigator.serviceWorker.register('/serviceWerx.js').then((registeredSW) => {
+    navigator.serviceWorker.register('~serviceWerx.js').then((registeredSW) => {
         console.log('Yeah bro its registered now', registeredSW)
     }).catch((err) => {
         console.log(`Nah bro, couldn\'t register it still! ${err}`);
